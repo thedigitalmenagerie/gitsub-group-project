e5883ed11f174f928f878ce3ab058acbcf9e218b
Dani Crosby <dani.crosby1111@gmail.com>
2021-02-14 19:35:20 -0600

    about me moved to js file

--- main.js
+++ main.js
@@ -590,7 +590,7 @@ const PrintNavBar = () => {
 		<div class="collapse navbar-collapse" id="navbarNavDropdown">
 			<ul class="navbar-nav">
 				<li class="nav-item active"><a class="nav-link" href="repo.html">Repositories</a></li>
-				<li class="nav-item"><a class="nav-link" href="projects.html">Projects <span class="sr-only"><img src="./images/github-cat4.webp" alt="github icon" id="navbar-icon"></a></span></a></li>
+				<li class="nav-item"><a class="nav-link" href="projects.html">Projects </a></li>
 				<li class="nav-item"><a class="nav-link" href="packages.html">Packages</a></li></ul></div></nav>
 </section>`;
 
@@ -613,6 +613,25 @@ const PrintFooter = () => {
 </ul>`;
 
 	PrintToDom('#footer-container', footer);
+};
+
+const PrintAboutMe = () => {
+	let about = `<div class="card text-white bg-dark" style="width: 54rem; font-size: 10px">
+	<div class="card-header p-2"><img src="./images/github-cat3.webp" alt="" style="width: 2em">  Dr.T / README.md</div>
+	<div class="info-card text-center" ><img src="./images/Dr.T-Pinned-Pic.png"alt="" style="width: 100%"></div>
+	<div class="card-body">
+			<p class="card-text">
+					"I hold a master’s degree in business administration and a doctorate of education with an emphasis in assistive technology and adult education.
+					My passion is studying how user and business needs collide into amazing opportunities for designers, developers, and users and how this 
+					collaborative relationship can inspire creativity and innovation. Especially for autistic users in the Black community."</p>
+	 
+	 <div class="container collaborate-container d-flex justify-content-center" style="height: 1.5em"id="collaborate-container">
+			<a href="https://github.com/drteresavasquez" style="color: hotpink; height: 1.5em; font-size: 14px"><p>. . : : : Collaborate With Me : : : . </p></a>
+	</div>
+</div>
+</div>`;
+
+	PrintToDom('#pinned-repo-container', about);
 };
 
 const PrintPinnedCards = () => {
@@ -953,6 +972,7 @@ const init = () => {
 	if (x === '/' || x === '/index.html') {
 		PrintNavBar();
 		PrintProfile();
+		PrintAboutMe();
 		PrintPinnedCards();
 		PrintFooter();
 	}
