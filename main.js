7feb778cda12c12de502f99c76ae912e2debaceb
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-02-14 18:53:43 -0600

    changed profile html to js and cleaned up code

--- main.js
+++ main.js
@@ -522,53 +522,65 @@ const arrProjects = [
 	}
 ];
 
-const PrintProjectProfile = () => {
-	let developerFormForm = ` <div class="container text-white bg-dark profile-container" id="profile-container">
- <img src="./images/dr.t recent.jpeg" id="profile-pic"alt="Photo of Dr. T.">
-	 <h>Dr. T.</h>
-	 <h6 id="handle">NSS</h6>
-	 <p id="profile-bio">Hello, My name is Dr. T. I'm a teacher at Nashville Software School, currently teaching E14.<p>
- <div class="container" id="profile-buttons">
-	 <a href="#" class="btn btn-warning btn-sm" id="follow-btn">Follow</a>
-	 <a href="#" class="btn btn-warning btn-sm" id="sponsor-btn">Sponsor</a>
-	 <a href="#" class="btn btn-warning btn-sm" id="more-btn">...</a>
-	 <br>
- </div>
- <a href="https://www.visitmusiccity.com/" style="color: white"><img src="./images/location-icon-256.webp" id="packages-icon-images">Nashville, TN</a>
- <a href="#" style="color: white"><img src="./images/email light.webp" id="packages-icon-images">DrT@gmail.com</a>
- <a href="https://www.drteresavasquez.com/" style="color: white"><img src="./images/link light.webp" id="packages-icon-images">drteresavasquez.com</a>
- <a href="https://twitter.com/home" style="color: white"><img src="./images/twitter light.webp" id="packages-icon-images">Twitter</a>
- <br>
-	 <h6>Highlights</h6>
-	 <a href="#" style="color: white"><img src="./images/_snowflake-256.webp" id="packages-icon-images">Arctic</a>
-	 <a href="#" style="color: white"><img src="./images/yellow star.webp" id="packages-icon-images">Github</a>
-	 <a href="#" style="color: white"><img src="./images/pro star light.webp" id="packages-icon-images">Pro</a>
-	 <br>
-	 <h6>Organizations</h6>
-	<div class="container" id="organization-profiles">
-	 <a href="https://github.com/nashville-software-school"><img src="./images/NSS-Logo.png" id="organization-icon-images" alt="NSS-Logo"></a>
-	 <a href="https://github.com/Urban-Green-Lab"><img src="./images/urban-green-icon.png" id="organization-icon-images" alt="urban-green-icon"></a>
-	 <a href="https://github.com/Autism-Possisble"><img src="./images/autism-possible.png" id="organization-icon-images" alt="autism-possible"></a>
-	 <a href="https://github.com/nss-evening-cohort-14"><img src="./images/cohort14.jpeg" id="organization-icon-images" alt="autism-possible"></a>
-	</div>
-		 <h6>Sponsors</h6>
-		<div class="container" id="sponsor-profiles">
-			<div class="sponsor-top-row">
-			 <img src="./images/honey-rae.jpeg" id="sponsor-icon-images" alt="placeholder image">
-			 <img src="./images/dani.jpeg" id="sponsor-icon-images" alt="placeholder image">
-			 <img src="./images/juan.jpeg" id="sponsor-icon-images" alt="placeholder image">
-			 <img src="./images/lindsey.jpeg" id="sponsor-icon-images" alt="placeholder image">
-		 </div>
-		 <div class="sponsor-bottom-row">
-			 <img src="./images/trinity.jpeg" id="sponsor-icon-images" alt="placeholder image">
-			 <img src="./images/Aja.jpeg" id="sponsor-icon-images" alt="placeholder image">
-			 <img src="./images/steve.png" id="sponsor-icon-images" alt="placeholder image">
-			 <img src="./images/wonder-woman.jpg" id="sponsor-icon-images" alt="placeholder image">
-		 </div>
-	 </div>
-`;
+const PrintProfile = () => {
+	let developerForm = `<div class="container text-white bg-dark profile-container" id="profile-container">
+  <img src="./images/Dr. T..jpg" id="profile-pic" alt="Photo of Dr. T.">
+  <h>Dr. T.</h>
+  <h7 id="handle">dope-2021</h7>
+  <p id="profile-bio">Hello, My name is Dr. T. I'm the best teacher at Nashville Software School.<p>
+          <div class="container" id="profile-buttons">
+              <a href="https://github.com/drteresavasquez?tab=followers" class="btn btn-secondary btn-sm" id="follow-btn">Follow</a>
+              <a href="#" class="btn btn-secondary btn-sm" id="sponsor-btn">Sponsor</a>
+              <a href="#" class="btn btn-secondary btn-sm" id="more-btn">...</a>
+              <br>
+          </div>
 
-	PrintToDom('#left-container', developerFormForm);
+          <div class="container" id="follow-links">
+              <a href="" style="color: white"><img src="./images/followers-1.webp" id="pinned-icons" style="background-color: lightgrey; border-radius: 50%">followers</a>
+              <a href="" style="color: white"><img src="./images/star-2.png" id="pinned-icons" style="background-color: lightgrey; border-radius: 50%">329</a>
+          </div>
+
+          <hr>
+          <a href="#" style="color: white"><img src="./images/locations-3.webp" id="pinned-icons" style="background-color: lightgrey; border-radius: 50%" > Tennessee, USA</a>
+          <a href="dr.teresavasquez@gmail.com" style="color: white"><img src="./images/email-2.webp" style="background-color: lightgrey; border-radius: 50%" id="pinned-icons"> dr.teresavasquez@gmail.com</a>
+          <a href="https://www.drteresavasquez.com/" style="color: white"><img src="./images/link-2.webp" style="background-color: lightgrey; border-radius: 50%" id="pinned-icons"> drteresavasquez.com/</a>
+          <a href="https://twitter.com/drteresavasquez" style="color: white"><img src="./images/twitter.webp" id="pinned-icons" style="background-color: lightgrey; border-radius: 50%" > twitter.com/drteresavasquez</a>
+
+         <hr>
+          <h7>Highlights</h7>
+          <a href="#" style="color: white"><img src="images/Arctic Code Vault.webp" id="pinned-icons" style="background-color: lightgrey; border-radius: 50%" >Arctic Code Vault Contributor</a>
+          <a href="#" style="color: white"><img src="images/Star (Filled in).webp" id="pinned-icons" style="background-color: lightgrey; border-radius: 50%" >Github Star</a>
+          <a href="#" style="color: white"><img src="images/Star.webp" id="pinned-icons" style="background-color: lightgrey; border-radius: 50%" >Pro</a>
+
+         <hr>
+          <h7>Organizations</h7>
+          <div class="container" id="organization-profiles">
+              <a href="https://github.com/nashville-software-school"><img src="./images/NSS-Logo.png" id="organization-icon-images" alt="NSS-Logo"></a>
+              <a href="https://github.com/Urban-Green-Lab"><img src="./images/urban-green-icon.png" id="organization-icon-images" alt="urban-green-icon"></a>
+              <a href="https://github.com/Autism-Possisble"><img src="./images/autism-possible.png" id="organization-icon-images" alt="autism-possible"></a>
+              <a href="https://github.com/nss-evening-cohort-14"><img src="./images/cohort14.jpeg" id="organization-icon-images" alt="autism-possible"></a>
+          </div>
+
+         <hr>
+          <h7>Sponsors</h7>
+          <div class="container" id="sponsor-profiles">
+              <div class="sponsor-top-row">
+                  <a href="https://github.com/lindseysatterfield"><img src="./images/lindsey.jpeg" id="sponsor-icon-images" alt="Lindsey Photo"></a>
+                  <a href="https://github.com/JuanDavila1101"></a><img src="./images/juan.jpeg" id="sponsor-icon-images" alt="Juan Photo">
+                  <a href="https://github.com/thedigitalmenagerie"><img src="./images/honey-rae.jpeg" id="sponsor-icon-images" alt="Honey-Rae Photo"></a>
+                  <a href="https://github.com/danicrosby"><img src="./images/dani.jpeg" id="sponsor-icon-images" alt="Dani Photo"></a>
+              </div>
+
+              <div class="sponsor-bottom-row">
+                  <a href="https://github.com/TrinityChristiana"><img src="./images/trinity.jpeg" id="sponsor-icon-images" alt="Trinity Photo"></a>
+                  <a href="https://github.com/ajawashington"><img src="./images/Aja.jpeg" id="sponsor-icon-images" alt="Aja Photo"></a>
+                  <a href="https://wonder-woman.fandom.com/wiki/Nubia"><img src="./images/nubia-wonderwoman.jpg" id="sponsor-icon-images" alt="placeholder image"></a>
+                  <a href="https://github.com/stevebrownlee"><img src="./images/steve.png" id="sponsor-icon-images" alt="Steve Photo"></a>
+              </div>
+        </div>
+  </div>`;
+
+	PrintToDom('#left-container', developerForm);
 };
 
 const PrintNavBar = () => {
@@ -924,23 +936,26 @@ const init = () => {
 
 	if (x === '/' || x === '/index.html') {
 		PrintNavBar();
+		PrintProfile();
 		PrintPinnedCards();
 		PrintFooter();
 	}
 	else if (x === '/repo.html') {
 		PrintNavBar();
+		PrintProfile();
 		PrintRepoCardsWithSearchBar();
 		PrintFooter();
 	}
 	else if (x === '/projects.html') {
 		PrintNavBar();
-		PrintProjectProfile();
+		PrintProfile();
 		PrintFooter();
 		PrintProjectsFormWithSearchBar(arrProjects);
 		newProjectFormCard();
 	}
 	else if (x === '/packages.html') {
 		PrintNavBar();
+		PrintProfile();
 		PrintPackagesCards();
 		PrintFooter();
 	}
